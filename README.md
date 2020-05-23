9d7a547b4a45771740a0e906bb4a2bc2ea4d4b91
Benny Samir Hierl <bennysamir@posteo.de>
2020-05-23 18:26:56 +0200

    input validation for share endpoint
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
 # Onvista-Share-Price-API
 
-ETF URL:
-https://onvista-share-price-api.now.sh/api/etf?ex=LSX&wkn={WKN}
+## URLs
+ETF: https://onvista-share-price-api.now.sh/api/etf?ex=LSX&wkn={WKN}
 
-Share URL:
-https://onvista-share-price-api.now.sh/api/share?ex=Lang%20%26%20Schwarz&wkn={WKN}
+Share: https://onvista-share-price-api.now.sh/api/share?ex=Lang%20%26%20Schwarz&wkn={WKN}
 
-## JSON Path for Portfolio Performance
+## Parameters
+* ex
+  * required with wkn
+  * not in combination with idNotation
+* wkn
+  * required with ex
+  * not in combination with idNotation
+* idNotation
+  * required
+  * not in combination with ex, wkn
+* datetimeTzStartRange
+  * optional
+  * format: DD.MM.YYYY
+* timeSpan
+  * optional
+  * format: 1Y,5Y,1M...
+
+## Examples
+
+``` https://onvista-share-price-api.now.sh/api/etf?ex=LSX&wkn={WKN}&timeSpan=5Y ```
+```https://onvista-share-price-api.now.sh/api/share?ex=Lang%20%26%20Schwarz&wkn={WKN}&timeSpan=5Y```
+
+
+## Portfolio Performance Setup
+### URLs
+ETF:
+``` https://onvista-share-price-api.now.sh/api/etf?ex=LSX&wkn={WKN} ```
+
+Share:
+```https://onvista-share-price-api.now.sh/api/share?ex=Lang%20%26%20Schwarz&wkn={WKN}```
+
+### JSON Path
 Date:
 ```
 $[*].datetimeLast.UTCTimeStamp
--- api/share.js
+++ api/share.js
@@ -2,92 +2,105 @@ const axios = require('axios');
 const csv = require("csvtojson");
 const cheerio = require('cheerio');
 const _ = require('lodash');
-
-const pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+const Joi = require('@hapi/joi');
+const schema = Joi.object({
+  wkn: Joi.string(),
+  ex: Joi.string(),
+  datetimeTzStartRange: Joi.string()
+    .pattern(new RegExp(/^(\d{2})\.(\d{2})\.(\d{4})$/))
+    .default('01.01.2019'),
+  timeSpan: Joi.string()
+    .pattern(new RegExp(/^[YMD][1]$/))
+    .replace(/^([1])([YMD])$/, '$2$1'),
+  idNotation: Joi.string()
+})
+.xor('wkn', 'idNotation')
+.xor('ex', 'idNotation')
+.with('wkn', 'ex')
+.with('ex', 'wkn')
 
 function toTimestamp(strDate) {
-  var d = new Date(strDate.replace(pattern, '$3-$2-$1'))
+  var d = new Date(strDate.replace(/(\d{2})\.(\d{2})\.(\d{4})/, '$3-$2-$1'))
   d.setHours(12)
   return d / 1000;
 }
 
 module.exports = async (req, res) => {
-  let notationId;
-  let interval;
-  const dateStart = req.query.dateStart || '01.01.2019'
-  if (req.query.interval) {
-    interval = `${req.query.interval.substring(1)}${req.query.interval.substring(0, 1)}`
-  } else {
-    interval = 'Y5'
-  }
+  try {
+    let query = await schema.validateAsync(req.query);
+    query.dateStart = query.datetimeTzStartRange
+    delete query.datetimeTzStartRange
+    query.interval = query.timeSpan
+    delete query.timeSpan
 
-  if (req.query.idNotation) {
-    notationId = req.query.idNotation
-  } else if (req.query.wkn && req.query.ex) {
-    const SearchReponse = await axios.get(`https://www.onvista.de/suche/?searchValue=${req.query.wkn}`)
-    const $ = cheerio.load(SearchReponse.data);
-    const exchanges = $('#exchangesLayer > ul > li > a').map((i, item) => {
-      let title = $(item).children().remove().end().text().trim()
-      let ex = $(item).children().remove().end().text().trim()
-      let idNotation = $(item).attr('href').match(/\?notation=([\d]*)/)[1]
+    if (query.idNotation) {
+      query.notationId = query.idNotation
+      delete query.idNotation
+    } else if (query.wkn && query.ex) {
+      const SearchReponse = await axios.get(`https://www.onvista.de/suche/?searchValue=${query.wkn}`)
+      const $ = cheerio.load(SearchReponse.data);
+      const exchanges = $('#exchangesLayer > ul > li > a').map((i, item) => {
+        let title = $(item).children().remove().end().text().trim()
+        let ex = $(item).children().remove().end().text().trim()
+        let idNotation = $(item).attr('href').match(/\?notation=([\d]*)/)[1]
+        return {
+          title,
+          ex,
+          idNotation
+        }
+      }).get()
+      const exhange = _.find(exchanges, function (o) {
+        return _.toUpper(o.ex) === _.toUpper(query.ex);
+      });
+      if (exhange === undefined) {
+        return res.status(400).send('idNotation konnte nicht ermittelt werden!')
+      }
+      query.notationId = exhange.idNotation
+      delete query.wkn
+      delete query.ex
+    }
+    console.log(query)
+    const {data} = await axios({
+      url: 'https://www.onvista.de/onvista/boxes/historicalquote/export.csv',
+      method: 'GET',
+      responseType: 'blob',
+      params: query
+    })
+    const datalist = await csv({
+      delimiter: [";"]
+    }).fromString(data.trimRight())
+    const responsedata =_.map(datalist, (i) => {
       return {
-        title,
-        ex,
-        idNotation
+        last: i.Schluss.replace(',', '.'),
+        first: i.Eroeffnung.replace(',', '.'),
+        high: i.Hoch.replace(',', '.'),
+        low: i.Tief.replace(',', '.'),
+        datetimeLast: {
+          localTime: "12:00:00 " + i.Datum,
+          localTimeZone: "GMT",
+          UTCTimeStamp: toTimestamp(i.Datum)
+        },
+        datetimeFirst: {
+          localTime: "12:00:00 " + i.Datum,
+          localTimeZone: "GMT",
+          UTCTimeStamp: toTimestamp(i.Datum)
+        },
+        datetimeHigh: {
+          localTime: "12:00:00 " + i.Datum,
+          localTimeZone: "GMT",
+          UTCTimeStamp: toTimestamp(i.Datum)
+        },
+        datetimeLow: {
+          localTime: "12:00:00 " + i.Datum,
+          localTimeZone: "GMT",
+          UTCTimeStamp: toTimestamp(i.Datum)
+        }
       }
-    }).get()
-    const exhange = _.find(exchanges, function (o) {
-      return _.toUpper(o.ex) === _.toUpper(req.query.ex);
-    });
-    if (exhange === undefined) {
-      return res.status(400).send('idNotation konnte nicht ermittelt werden!')
-    }
-    notationId = exhange.idNotation
-  } else {
-    return res.status(400).send('idNotation oder WKN + EX müssen übergeben werden!')
+    })
+    res.send(responsedata)
+  } catch (error) {
+    console.error(error)
+    res.status(400).json(error)
   }
-
-  const {data} = await axios({
-    url: 'https://www.onvista.de/onvista/boxes/historicalquote/export.csv',
-    method: 'GET',
-    responseType: 'blob',
-    params: {
-      dateStart,
-      interval,
-      notationId
-    }
-  })
-  const datalist = await csv({
-    delimiter: [";"]
-  }).fromString(data.trimRight())
-  const responsedata =_.map(datalist, (i) => {
-    return {
-      last: i.Schluss.replace(',', '.'),
-      first: i.Eroeffnung.replace(',', '.'),
-      high: i.Hoch.replace(',', '.'),
-      low: i.Tief.replace(',', '.'),
-      datetimeLast: {
-        localTime: "12:00:00 " + i.Datum,
-        localTimeZone: "GMT",
-        UTCTimeStamp: toTimestamp(i.Datum)
-      },
-      datetimeFirst: {
-        localTime: "12:00:00 " + i.Datum,
-        localTimeZone: "GMT",
-        UTCTimeStamp: toTimestamp(i.Datum)
-      },
-      datetimeHigh: {
-        localTime: "12:00:00 " + i.Datum,
-        localTimeZone: "GMT",
-        UTCTimeStamp: toTimestamp(i.Datum)
-      },
-      datetimeLow: {
-        localTime: "12:00:00 " + i.Datum,
-        localTimeZone: "GMT",
-        UTCTimeStamp: toTimestamp(i.Datum)
-      }
-    }
-  })
-  res.send(responsedata)
 }
 
